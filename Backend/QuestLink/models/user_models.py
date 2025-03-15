23b01020ab655307940a416b372d93927cce3a01
Piotr Janiszek <pietrasos567@gmail.com>
2025-03-15 01:10:54 +0100

    Split models into separate files

---
+++ Backend/QuestLink/models/user_models.py
@@ -0,0 +1,23 @@
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = sa.Column(sa.Integer, primary_key=True)
+    username = sa.Column(sa.String(255), nullable=False)
+    email = sa.Column(sa.String(255), nullable=False)
+    is_active = sa.Column(sa.Boolean, default=True)
+    is_verified = sa.Column(sa.Boolean, default=False)
+    created_at = sa.Column(sa.DateTime, server_default=sa.func.now())
+    updated_at = sa.Column(sa.DateTime, server_default=sa.func.now(), onupdate=sa.func.now())
+
+
+class Account(Base):
+    __tablename__ = "accounts"
+    id = sa.Column(sa.Integer, primary_key=True)
+    user_id = sa.Column(sa.Integer, sa.ForeignKey("users.id"), nullable=False)
+    login = sa.Column(sa.String(255), nullable=False)
+    password = sa.Column(sa.String(255), nullable=False)
+    is_superuser = sa.Column(sa.Boolean, default=False)
